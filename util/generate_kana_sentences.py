d17236946336e95427064338306a970a4aa1faac
Conrado P. L. Gouvea <conradoplg@gmail.com>
2018-01-17 22:04:22 -0200

    Add a bit more work on the parser; some tests

--- util/generate_kana_sentences.py
+++ util/generate_kana_sentences.py
@@ -1,8 +1,19 @@
 import re
 
-_WORD_RE = re.compile(r'^(\w+)(\|\d+)?(\[\d+\])?(\{\w+\})?~?')
+_WORD_RE = re.compile(r'^(\w+)(\|\d+)?(\(\w+\))?(\[\d+\])?(\{\w+\})?~?')
+_KANA_RE = re.compile(r'^[\u30a0-\u30ff\u3040-\u309f]+$')
+
+
+def is_kana(s):
+    return _KANA_RE.match(s)
+
 
 def get_word_kana(word):
+    """
+    Given a "word", return it as kana.
+
+    Uses edict2.
+    """
     regex = re.compile(r'^' + word + r'\W[^\[]+\[([^\]]+)\]')
     with open('util/edict2', encoding='utf-8') as f:
         for line in f:
@@ -11,20 +22,28 @@ def get_word_kana(word):
                 return m.group(1)
     return None
 
+
 def parse_full_word(full_word):
+    """
+    Parse a Tanaka corpus line B "full word" (e.g. "此れ[01]{これ}").
+    """
     m = _WORD_RE.match(full_word)
     if not m:
         raise RuntimeError('Unable to match: ' + full_word)
-    word, _, sense, reading = m.groups()
-    if sense:
-        sense = sense[1:-1]
+    word, _, reading, sense, form = m.groups()
     if reading:
         reading = reading[1:-1]
-    return word, sense, reading
+    if sense:
+        sense = sense[1:-1]
+    if form:
+        form = form[1:-1]
+    return word, reading, sense, form
 
 
 def convert_word(full_word):
-    word, sense, reading = parse_full_word(full_word)
+    word, reading, sense, form = parse_full_word(full_word)
+    if form and is_kana(form):
+        return form
 
 
 def parse_jpn_indices_line(line):
@@ -50,3 +69,10 @@ def main():
 if __name__ == '__main__':
     # main()
     print(get_word_kana('此れ'))
+    assert get_word_kana('此れ') == 'これ'
+    assert parse_full_word('物(もの)[01]{もの}') == ('物', 'もの', '01', 'もの')
+    assert is_kana('ぁぬゖァヴ')
+    assert not is_kana('abc')
+    assert not is_kana('ぁabc')
+    assert not is_kana('abcぁ')
+    assert convert_word('住む{住んでいる}') == 'すんでいる'
